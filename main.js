d387deeba8a65f07a67d96fa68c3aff533bc2df4
Sa'dullojon <148865375+sadulloradjabov@users.noreply.github.com>
2023-11-16 13:31:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// let son = 0
+
+// while (son < 3) {
+//     alert(son);
+//     son = son + 1
+// }
+
+
+// let son = 0
+
+// while (son <= 100) {
+//     console.log(son)
+//     son = son + 2;
+// }
+
+
+// let a = 2
+// let b = 9
+
+// while () {
+    
+// }
+
+
+let x = "#"
+while (x <= "##########") {
+    console.log(x)
+    x = x + "#"
+}
